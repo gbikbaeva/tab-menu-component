a480855b8ee997fe63bcf7b9452301d052b7f7c1
Gulnur Bikbaeva <g.bikbaeva@crif.com>
2025-08-26 12:01:42 +0200

    add event listeners

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
+document.querySelectorAll('.tabs__chip').forEach(tabChip => {
+  tabChip.addEventListener('click', () => {
+    const value = tabChip.getAttribute('data-value');
+
+    document.querySelectorAll('.tabs__chip').forEach(item => {
+      const isSelected = item.getAttribute('data-value') === value;
+      item.classList.toggle('active', isSelected);
+      item.setAttribute('aria-selected', isSelected);
+    });
+
+    document.querySelectorAll('.tabs__panel').forEach(tabPanel => {
+      tabPanel.hidden = tabPanel.getAttribute('data-value') !== value;
+    });
+  });
+});
